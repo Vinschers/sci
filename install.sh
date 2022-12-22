a4720f899e06e3cef9e73c09935e3450d19a36d5
Scherer <vincentin.py@gmail.com>
2022-12-22 20:12:56 -0300

    Fix ln

--- install.sh
+++ install.sh
@@ -11,4 +11,4 @@ fi
 
 sudo patch -N /usr/lib/node_modules/translation-server/modules/utilities/utilities_item.js utilities_item.js.diff
 
-sudo ln -s /usr/share/sci /usr/local/bin/sci
+sudo ln -s /usr/share/sci/sci /usr/local/bin/sci
